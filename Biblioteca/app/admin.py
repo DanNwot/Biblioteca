add4b6fe96a76da2a19f250d2dcdc66204bffab1
Daniel Godoi de Morais <danielgodoim1@gmail.com>
2026-03-16 15:48:37 -0300

    Add files via upload

---
+++ Biblioteca/app/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import *
+
+class LivroInline(admin.TabularInline):
+    model = Livro
+    extra = 1
+
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+    inlines = [LivroInline]
+
+
+admin.site.register(Cidade)
+admin.site.register(Editora)
+admin.site.register(Leitor)
+admin.site.register(Genero)
+admin.site.register(Livro)
+admin.site.register(Autor, AutorAdmin)
---
+++ Biblioteca/app/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.contrib import messages
+
+from .models import *
+from .forms import LivroForm
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        livros = Livro.objects.all()
+        return render(request, 'index.html', {'livros': livros})
+
+
+class LivrosView(View):
+    def get(self, request, *args, **kwargs):
+        livros = Livro.objects.all()
+        return render(request, 'livros.html', {'livros': livros})
+
+
+class CidadesView(View):
+    def get(self, request, *args, **kwargs):
+        cidades = Cidade.objects.all()
+        return render(request, 'cidade.html', {'cidades': cidades})
+
+
+class AutoresView(View):
+    def get(self, request, *args, **kwargs):
+        autores = Autor.objects.all()
+        return render(request, 'autor.html', {'autores': autores})
+
+
+class EditorasView(View):
+    def get(self, request, *args, **kwargs):
+        editoras = Editora.objects.all()
+        return render(request, 'editora.html', {'editoras': editoras})
+
+
+class LeitoresView(View):
+    def get(self, request, *args, **kwargs):
+        leitores = Leitor.objects.all()
+        return render(request, 'leitor.html', {'leitores': leitores})
+
+
+class GenerosView(View):
+    def get(self, request, *args, **kwargs):
+        generos = Genero.objects.all()
+        return render(request, 'genero.html', {'generos': generos})
+
+
+class EditarLivroView(View):
+    template_name = 'editar_livro.html'
+
+    def get(self, request, id, *args, **kwargs):
+        livro = get_object_or_404(Livro, id=id)
+        form = LivroForm(instance=livro)
+        return render(request, self.template_name, {'livro': livro, 'form': form})
+
+    def post(self, request, id, *args, **kwargs):
+        livro = get_object_or_404(Livro, id=id)
+        form = LivroForm(request.POST, instance=livro)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'As edições foram salvas com sucesso.')
+            return redirect('editar', id=id)
+        else:
+            messages.error(request, 'Corrija os erros no formulário antes de enviar novamente.')
+            return render(request, self.template_name, {'livro': livro, 'form': form})
+
+
+class DeleteLivroView(View):
+    def get(self, request, id, *args, **kwargs):
+        livro = get_object_or_404(Livro, id=id)
+        livro.delete()
+        messages.success(request, 'Livro excluído com sucesso.')
+        return redirect('index')
